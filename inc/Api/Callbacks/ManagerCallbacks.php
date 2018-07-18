c23db4ad0858cedaaebe2ad6fffb5a424575089a
heroesaddict <chattokim@gmail.com>
2018-07-18 18:05:06 +0800

    Part 23 - Save Default Options on Activate

--- inc/Api/Callbacks/ManagerCallbacks.php
+++ inc/Api/Callbacks/ManagerCallbacks.php
@@ -30,6 +30,8 @@ class ManagerCallbacks extends BaseController
 		$classes = $args['class'];
 		$option_name = $args['option_name'];
 		$checkbox = get_option( $option_name );
-		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ($checkbox[$name] ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
+		$checked = isset($checkbox[$name]) ? ($checkbox[$name] ? true : false) : false;
+
+		echo '<div class="' . $classes . '"><input type="checkbox" id="' . $name . '" name="' . $option_name . '[' . $name . ']" value="1" class="" ' . ( $checked ? 'checked' : '') . '><label for="' . $name . '"><div></div></label></div>';
 	}
 }
--- inc/Base/Activate.php
+++ inc/Base/Activate.php
@@ -7,7 +7,15 @@ namespace Inc\Base;
 //name of class and file should match for psr-4 autoload to work
 class Activate
 {
-    public static function activate() {
-        flush_rewrite_rules();
-    }
+	public static function activate() {
+		flush_rewrite_rules();
+
+		if ( get_option( 'alecaddd_plugin' ) ) {
+			return;
+		}
+
+		$default = array();
+
+		update_option( 'alecaddd_plugin', $default );
+	}
 }
